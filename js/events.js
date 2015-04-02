4b722b7bd4eedb6bc9cc7e841be3c9904c2a4b71
kikill95 <kirill.gus@gmail.com>
2015-04-02 21:53:41 +0300

    Adding styles for images in lists

--- js/events.js
+++ js/events.js
@@ -1,5 +1,6 @@
 (function() {
     $('.carousel img').css('height', $('body').css('height'));
+    //because we don't want to change image size on zooming
     $(window).on('resize', function() {
         $('.carousel img').css('height', $('body').css('height'));
     });
