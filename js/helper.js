d4f7b74e9a0e889eb14ae8a5e0a49da3423bb975
kikill95 <kirill.gus@gmail.com>
2015-04-03 23:54:50 +0300

    Overflow: hidden

--- js/helper.js
+++ js/helper.js
@@ -3,5 +3,6 @@ styling();
 function styling() {
     var height = parseInt($('body').css('height'));
     $('.carousel .element').css('height', height);
+    $('.carousel .pages').css('height', height);
     $('.indicators.pages-indicator').css('top', height / 2 - $('.page-indicator').length * parseInt($('.page-indicator').css('height')) + 'px');
 }
