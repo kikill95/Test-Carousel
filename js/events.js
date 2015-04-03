3502e2a774998215fced58d28a782f92cc540350
kikill95 <kirill.gus@gmail.com>
2015-04-03 10:17:36 +0300

    Adding some mouse events

--- js/events.js
+++ js/events.js
@@ -1,9 +1,35 @@
 (function() {
-    $('.carousel .element').css('height', $('body').css('height'));
-    //because we don't want to change image size on zooming
-//    $(window).on('resize', function() {
-//        $('.carousel .element').css('height', $('body').css('height'));
-//    });
+    var firstPositionX = 0,
+        firstPositionY = 0,
+        $element = $('.carousel .element'),
+        $elements = $('.carousel .elements');
+
+    $element.css('height', $('body').css('height'))
+        .on('mousedown', function(e) {
+            firstPositionX = e.clientX;
+            firstPositionY = e.clientY;
+        })
+        .on('mouseup', function(e) {
+            var secondPositionX = e.clientX,
+                secondPositionY = e.clientY;
+            if ( Math.abs(secondPositionX - firstPositionX) > parseInt($element.css('width')) / 4 ||
+                    Math.abs(secondPositionY - firstPositionY) > parseInt($element.css('height')) / 4 ) {
+
+                if (Math.abs(secondPositionX - firstPositionX) > Math.abs(secondPositionY - firstPositionY)) {
+                    console.log('left-right');
+                } else {
+                    console.log('up-down');
+                }
+
+            }
+        })
+        .on('dragstart', function(e) {
+            e.preventDefault();
+        });
+    //because we don't want to change images size on zooming
+    $(window).on('resize', function() {
+        $element.css('height', $('body').css('height'));
+    });
 
 
 
